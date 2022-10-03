5513d7c67d304d3ddaf271d723951769a9e813a9
Vuk Vidacic <vidacicvuk13@gmail.com>
2022-10-03 07:54:18 +0200

    align fix

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -11,7 +11,7 @@ export const GridContainer = styled.section`
 display: grid;
 grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
 padding: 3rem;
-place-items: center;
+place-items: start end;
 column-gap: 2rem;
 row-gap: 3rem;
 @media ${(props) => props.theme.breakpoints.sm} {
