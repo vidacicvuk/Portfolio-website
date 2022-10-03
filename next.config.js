ad5a2cd37b67272101a0799b67f5c33e98d49eb8
Vuk Vidacic <vidacicvuk13@gmail.com>
2022-10-03 06:33:14 +0200

    add basepath

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+
+const NextConfig ={
+    reactStrictMode: true;
+    images: {
+        loader: "akamai",
+        path: ""
+    },
+    basePath: "/nextjs-pages",
+    assetPrefix: "/nextjs-pages"
+};
